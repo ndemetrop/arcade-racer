1d8d585700ae9f8d38d44b8896a66b0ca5960c86
ndemetrop <nicholas.demetropoulos@gmail.com>
2025-03-16 21:17:59 -0400

    Revert "Prototype source code"
    This reverts commit 69f043f81c1a0e9dace6d9e0a3ccdc268fbb3f03.

--- prototype/source/Source/arcade_racer/arcade_racerGameMode.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "arcade_racerGameMode.h"
-#include "arcade_racerPlayerController.h"
-
-Aarcade_racerGameMode::Aarcade_racerGameMode()
-{
-	PlayerControllerClass = Aarcade_racerPlayerController::StaticClass();
-}
